145b1fae9bcc1d13b430267b0e9dc17199500a3f
Gianluis99 <gianluigipanza31@gmail.com>
2021-10-28 09:11:58 +0200

    add sort method

---
+++ src/main/java/prova/intermedia/unical/it/ProvaIntermediaIngsw/MyListUtil.java
@@ -0,0 +1,27 @@
+package prova.intermedia.unical.it.ProvaIntermediaIngsw;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MyListUtil 
+{
+      public List<Integer> sort(List<Integer> list, final boolean value){
+		
+    	  list.sort(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+			
+				if(value)
+					return o1-o2;
+				else
+					return o2-o1;
+			}
+		
+    	  
+    	  });
+    	  
+    	  return list;
+    	  
+      }
+}
